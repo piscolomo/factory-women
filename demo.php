5735574d25ed5b4dd8e70e5ce453db2af2532834
Julio <ljuliom@gmail.com>
2014-08-23 15:45:25 -0500

    insert post content, tests

--- demo.php
+++ demo.php
@@ -20,7 +20,7 @@ if( ! array_key_exists( 'factory-women', $GLOBALS ) ) {
           if (function_exists('add_action')) {
 						add_action('admin_menu', array($this, 'add_page')); 
 					}
-        } // end constructor
+        }
 
         function add_page(){	
 					if (function_exists('add_options_page')) {
@@ -32,26 +32,32 @@ if( ! array_key_exists( 'factory-women', $GLOBALS ) ) {
 					include('template/index.html');
 					$numberposts = $_POST['numberposts'];
 					$posttitle = $_POST['posttitle'];
+					$postcontent = $_POST['postcontent'];
 					if(isset($numberposts)){
-						$this->insert_posts($numberposts, $posttitle);
+						$this->insert_posts($numberposts, $posttitle, $postcontent);
 					}
 				}
 
-				public function insert_posts($n, $title){
+				public function insert_posts($n, $title, $postcontent){
 					for ($i=1; $i<=$n; $i++){
 						$post = array(
 							'post_title' => "$title $i",
 							'post_type' => "post",
+							'post_content'  => $postcontent==null ? $this->getlipsum() : $postcontent,
 							'post_status' => "publish"
 						);
 						wp_insert_post( $post );
 					}
 				}
 
+				public function getlipsum(){
+					return "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod nisi risus, id bibendum eros mattis quis. Donec commodo pulvinar massa ac viverra. Nullam posuere, massa sit amet ultrices viverra, dolor ipsum volutpat ante, et posuere libero lacus ac risus. Cras nec risus vitae arcu egestas sollicitudin non ac quam. Integer laoreet, lorem et aliquam feugiat, magna purus bibendum nulla, eget consequat quam nisi sed erat. Etiam feugiat eleifend dictum. Fusce cursus nibh quis nunc rhoncus, nec posuere felis molestie. Mauris viverra ipsum eget augue sodales, id consectetur dolor pellentesque.";
+				}
+
        
-    } // end class
+    }
      
     // Store a reference to the plugin in GLOBALS so that our unit tests can access it
     $GLOBALS['factory-women'] = new FactoryWomen();
      
-} // end if
+}
--- tests/test-sample.php
+++ tests/test-sample.php
@@ -7,7 +7,6 @@ class FactoryWomen_Tests extends WP_UnitTestCase {
     function setUp() {     
         parent::setUp();
         $this->plugin = $GLOBALS['factory-women'];
-        $this->plugin->insert_posts(5,"prueba");
     }
      
     function testPluginInitialization() {
@@ -15,12 +14,26 @@ class FactoryWomen_Tests extends WP_UnitTestCase {
     }
 
     function testInsertPosts() {
+        $this->plugin->insert_posts(5,"prueba", "");
         $this->assertEquals( 5, wp_count_posts()->publish);   
     }
 
     function testLastPostContainsTitle() {
+        $this->plugin->insert_posts(5,"prueba", "");
         $lastpost = wp_get_recent_posts(array( 'numberposts' => '1', 'order' => 'DESC'));
         $this->assertContains("prueba", $lastpost[0]["post_title"]);
-    } 
+    }
+
+    function testLastPostContainsContentLipsum(){
+        $this->plugin->insert_posts(5,"prueba", "");
+        $lastpost = wp_get_recent_posts(array( 'numberposts' => '1', 'order' => 'DESC'));
+        $this->assertContains("Lorem ipsum dolor", $lastpost[0]["post_content"]);
+    }
+
+    function testLastPostContainsRightContent(){
+        $this->plugin->insert_posts(5,"prueba", "Este es el contenido de prueba");
+        $lastpost = wp_get_recent_posts(array( 'numberposts' => '1', 'order' => 'DESC'));
+        $this->assertContains("Este es el contenido de prueba", $lastpost[0]["post_content"]);
+    }
      
 }
