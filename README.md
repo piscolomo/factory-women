ae625c86430a91f5b3d5c8213d3052dce1912e16
Julio <ljuliom@gmail.com>
2014-08-26 12:11:02 -0500

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 factory-women
 =============
 
-A Wordpress's plugin to setting up posts as test data
+A Wordpress's plugin to setting up posts as test data. You can create posts or pages, indicating number of items, their titles and content.
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,7 +7,7 @@ if ( !$_tests_dir ) $_tests_dir = '/../../../../wordpress-develop/tests/phpunit'
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../demo.php';
+	require dirname( __FILE__ ) . '/../factorywomen.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
