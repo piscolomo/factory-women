3f0ed91187ac03fb7885ca09c5aaf106d79a740c
Julio <ljuliom@gmail.com>
2014-08-26 11:23:01 -0500

    context fake function

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -2,20 +2,13 @@
 
 class FactoryWomen_Tests extends WP_UnitTestCase {
  
-    private $plugin;
- 
-    function setUp() {     
+    function setUp() {   
         parent::setUp();
         $this->plugin = $GLOBALS['factory-women'];
     }
-     
+
     function testPluginInitialization() {
         $this->assertFalse( null == $this->plugin );
-    }
-
-    function testInsertPosts() {
-        $this->plugin->insert_posts(5,"post","prueba", "");
-        $this->assertEquals( 5, wp_count_posts()->publish);   
     }
 
     function testInsertPages() {
@@ -23,17 +16,26 @@ class FactoryWomen_Tests extends WP_UnitTestCase {
         $this->assertEquals( 5, wp_count_posts('page')->publish);   
     }
 
-    function testLastPostContainsTitle() {
+    function ContextPostsWithoutContent(){
         $this->plugin->insert_posts(5,"post","prueba", "");
-        $lastpost = wp_get_recent_posts(array( 'numberposts' => '1', 'order' => 'DESC'));
-        $this->assertContains("prueba", $lastpost[0]["post_title"]);
     }
 
-    function testLastPostContainsContentLipsum(){
-        $this->plugin->insert_posts(5,"post","prueba", "");
-        $lastpost = wp_get_recent_posts(array( 'numberposts' => '1', 'order' => 'DESC'));
-        $this->assertContains("Lorem ipsum dolor", $lastpost[0]["post_content"]);
-    }
+        function testInsertPosts() {
+            $this->ContextPostsWithoutContent();
+            $this->assertEquals( 5, wp_count_posts()->publish);   
+        }
+
+        function testLastPostContainsTitle() {
+            $this->ContextPostsWithoutContent();
+            $lastpost = wp_get_recent_posts(array( 'numberposts' => '1', 'order' => 'DESC'));
+            $this->assertContains("prueba", $lastpost[0]["post_title"]);
+        }
+
+        function testLastPostContainsContentLipsum(){
+            $this->ContextPostsWithoutContent();
+            $lastpost = wp_get_recent_posts(array( 'numberposts' => '1', 'order' => 'DESC'));
+            $this->assertContains("Lorem ipsum dolor", $lastpost[0]["post_content"]);
+        }
 
     function testLastPostContainsRightContent(){
         $this->plugin->insert_posts(5,"post","prueba", "Este es el contenido de prueba");
